42bdeffe699a9fa72b74e7fb08c87727192ad060
Emily Long <emilylong17@gmail.com>
2017-05-10 23:27:09 -0400

    moved some files around, added mid.jsons, burden_mid map

---
+++ wireframe/js/burden_mid.js
@@ -0,0 +1,320 @@
+// Danya Littlefield, Phoebe Holtzman, Emily Long
+// js for pa housing need
+// THIS IS OUR SETUP
+var margin = {top: 30, right: 20, bottom: 30, left: 50},
+    width = 400 - margin.left - margin.right,
+    height = 220 - margin.top - margin.bottom;
+
+var projection = d3.geoAlbers()
+    .scale(3500)
+    .rotate( [77.8367,0] )
+    .center( [0, 40.8766] )
+    .translate([width / 2, height / 2]);
+
+var path = d3.geoPath()
+    .projection(projection);
+
+// Slider 
+
+var inputValue = null;
+var year = ["2005","2010","2015"];    
+
+// var svg = d3.select("#mapRbur").append("svg")
+//     .attr("width", width + margin.left + margin.right)
+//     .attr("height", height + margin.top + margin.bottom)
+
+// var svg2 = d3.select("#mapHbur").append("svg")
+//     .attr("width", width + margin.left + margin.right)
+//     .attr("height", height + margin.top + margin.bottom);
+
+var color = d3.scaleThreshold()
+    .domain([20, 30, 40, 60, 80])
+    .range(['#feedde','#fdbe85','#fd8d3c','#e6550d', '#a63603']);
+
+var colorh = d3.scaleThreshold()
+    .domain([20, 30, 40, 60, 80])
+    .range(['#eff3ff','#bdd7e7','#6baed6','#3182bd', '#08519c']);
+
+
+// INITIAL QUEUE //
+// Queue up datasets using d3 Queue. Prevents errors from loading
+d3.queue()
+    .defer(d3.json, "pa_pumas_05_mid.json") // Load US PUMAs geography data
+    .defer(d3.json, "counties16.json")
+    .await(loadpage); 
+
+    
+// UPDATE DATA FUNCTIONS //
+// Run 'ready' when JSONs are loaded
+// Update Function, runs when data is loaded 
+function loadpage(error, pa_pumas_05_mid, counties16) {
+    if (error) throw error;
+
+    d3.selectAll("path").remove();
+
+    var mapcontainer = d3.select("#mapRbur")
+
+    var mymap = mapcontainer.append("svg")
+          .selectAll("path")
+            .data(topojson.feature(pa_pumas_05_mid, pa_pumas_05_mid.objects.pa_pumas_05_mid).features) // Bind TopoJSON data elements
+        .enter().append("path")
+            .attr("d", path)
+        .attr("class", "renters2005")
+        .style("fill", function(d) { 
+            if (d.properties.r05_30 > 0) {
+            return color(d.properties.r05_30);
+            } else {
+            return "white";
+          };
+        })
+        .on("mouseover", function(d){
+          console.log("mouseover");
+        return tooltip.style("visibility", "visible").text("% Burdened at 50%:" + Math.round(d.properties.h05_30) +"%");
+        })
+        .on("mousemove", function(d){
+          console.log("mousemove");
+        return tooltip.style("top", (event.pageY-10)+"px").style("left",(event.pageX+10)+"px").text(Math.round(d.properties.h05_30) +"%");
+          // + "\n"+ "% Burdened at 50%:" + Math.round(d.properties.h05_50) +"%");
+        })
+        .on("mouseout", function(d){
+          console.log("mouseout");
+          div.style("opacity", 0);
+          return tooltip.style("visibility", "hidden");
+        });
+    
+    d3.select("#mapHbur").append("svg")
+      .selectAll("path")
+          .data(topojson.feature(pa_pumas_05_mid, pa_pumas_05_mid.objects.pa_pumas_05_mid).features) // Bind TopoJSON data elements
+      .enter().append("path")
+          .attr("d", path)
+      .attr("class", "owners2005")
+      .style("fill", function(d) { 
+          if (d.properties.h05_30 > 0) {
+          return colorh(d.properties.h05_30);
+          } else {
+          return "#FFF";
+          } 
+      // })
+      // .on("mouseover", function(d){
+      //   console.log(d.properties.id);
+      // return tooltip.style("visibility", "visible").text(d.properties.id);
+      // })
+      // .on("mousemove", function(d){
+      //   console.log(d.properties.id);
+      //   return tooltip.style("top", (event.pageY-10)+"px").style("left",(event.pageX+10)+"px").text(d.properties.id);
+      // })
+      // .on("mouseout", function(d){
+      //   return tooltip.style("visibility", "hidden");
+      // .on("mouseover", function(d){
+      //   return tooltipH.style("visibility", "visible").text("PUMA ID: " + d.properties.id + "\n"+ "% Burdened at 50%:" + Math.round(d.properties.h05_50) +"%");
+      // })
+      // .on("mousemove", function(d){
+      //   return tooltipH.style("top", (event.pageY-10)+"px").style("left",(event.pageX+10)+"px").text("PUMA ID: " + d.properties.id + "\n" + "% Burdened at 50%: " + Math.round(d.properties.h05_50) + "%");
+      // })
+      // .on("mouseout", function(d){
+      //   return tooltipH.style("visibility", "hidden");
+      });
+
+var tooltip = mapcontainer.append("div")
+    .attr('class', 'tooltip')
+
+    // create county outlines
+    drawCounties(counties16)
+
+    d3.select("#timeslide").on("input", function() {
+      update(this.value);
+    });
+
+    function update(value) {
+      document.getElementById("range").innerHTML=year[value];
+      inputValue = year[value];
+      updateYear(inputValue);
+    };
+}
+//end of update function
+
+function updateYear(value) {
+  console.log("Updated to year:");
+  if (value == "2015") {
+    d3.queue()
+    .defer(d3.json, "pa_pumas_16_mid.json") // Load US PUMAs geography data
+    .defer(d3.json, "counties16.json")
+    .await(update2015); 
+    //update2015();
+    console.log(value);
+  } else if (value == "2010") {
+    d3.queue()
+    .defer(d3.json, "pa_pumas_05_mid.json")  // Load US PUMAs geography data
+    .defer(d3.json, "counties16.json")
+    .await(update2010); 
+    //update2010();
+    console.log(value);
+  } else {
+    d3.queue()
+    .defer(d3.json, "pa_pumas_05_mid.json") // Load US PUMAs geography data
+    .defer(d3.json, "counties16.json")
+    .await(update2005); 
+    //loadpage();
+    console.log(value);
+  }
+}
+
+function drawCounties(counties16) {
+  d3.selectAll("svg")
+  .append("path")
+  .datum(topojson.mesh(counties16, counties16.objects.counties))
+  .attr("d", path)
+  .attr("class", "counties");
+}
+
+function update2010(error, pa_pumas_05_mid, counties16) { // initial creation
+    if (error) throw error;
+    //clear paths
+    d3.selectAll("path").remove();
+
+    d3.select("#mapRbur").select("svg")
+        .selectAll("path")
+            .data(topojson.feature(pa_pumas_05_mid, pa_pumas_05_mid.objects.pa_pumas_05_mid).features) // Bind TopoJSON data elements
+        .enter().append("path")
+            .attr("d", path)
+        .attr("class", "renters2010")
+        .style("fill", function(d) { 
+            if (d.properties.r10_30 > 0) {
+            return color(d.properties.r10_30);
+            } else {
+            return "#FFF";
+          }; 
+    })
+
+    d3.select("#mapHbur").select("svg")
+      .selectAll("path")
+          .data(topojson.feature(pa_pumas_05_mid, pa_pumas_05_mid.objects.pa_pumas_05_mid).features) // Bind TopoJSON data elements
+      .enter().append("path")
+          .attr("d", path)
+      .attr("class", "owners2010")
+      .style("fill", function(d) { 
+          if (d.properties.h10_30 > 0) {
+          return colorh(d.properties.h10_30);
+          } else {
+          return "#FFF";
+          };
+    })
+
+    // create county outlines
+    drawCounties(counties16);
+
+};
+
+function update2005(error, pa_pumas_05_mid, counties16) { // initial creation
+    if (error) throw error;
+    
+    d3.selectAll("path").remove();
+
+    console.log("update 2005 running");
+
+    d3.select("#mapRbur").select("svg")
+        .selectAll("path")
+            .data(topojson.feature(pa_pumas_05_mid, pa_pumas_05_mid.objects.pa_pumas_05_mid).features) // Bind TopoJSON data elements
+        .enter().append("path")
+            .attr("d", path)
+        .attr("class", "renters2005")
+        .style("fill", function(d) { 
+            if (d.properties.r05_30 > 0) {
+            return color(d.properties.r05_30);
+            } else {
+            return "#FFF";
+          }  
+        });
+    
+    d3.select("#mapHbur").select("svg")
+      .selectAll("path")
+          .data(topojson.feature(pa_pumas_05_mid, pa_pumas_05_mid.objects.pa_pumas_05_mid).features) // Bind TopoJSON data elements
+      .enter().append("path")
+          .attr("d", path)
+      .attr("class","owners2015")
+      .style("fill", function(d) { 
+          if (d.properties.h05_30 > 0) {
+          return colorh(d.properties.h05_30);
+          } else {
+          return "#FFF";
+          } 
+      });
+
+    // create county outlines
+    drawCounties(counties16);
+};
+
+function update2015(error, pa_pumas_16_mid, counties16) { // initial creation
+    if (error) throw error;
+    
+    d3.selectAll("path").remove();
+
+    console.log("update 2015 running");
+
+    d3.select("#mapRbur").select("svg")
+        .selectAll("path")
+            .data(topojson.feature(pa_pumas_16_mid, pa_pumas_16_mid.objects.pa_pumas_16_mid).features) // Bind TopoJSON data elements
+        .enter().append("path")
+            .attr("d", path)
+        .attr("class", "renters2015")
+        .style("fill", function(d) { 
+            if (d.properties.r15_30 > 0) {
+            return color(d.properties.r15_30);
+            } else {
+            return "#FFF";
+          }  
+        });
+    
+    d3.select("#mapHbur").select("svg")
+      .selectAll("path")
+          .data(topojson.feature(pa_pumas_16_mid, pa_pumas_16_mid.objects.pa_pumas_16_mid).features) // Bind TopoJSON data elements
+      .enter().append("path")
+          .attr("d", path)
+      .attr("class","owners2015")
+      .style("fill", function(d) { 
+          if (d.properties.h15_30 > 0) {
+          return colorh(d.properties.h15_30);
+          } else {
+          return "#FFF";
+          } 
+      });
+
+    // create county outlines
+    drawCounties(counties16);
+};
+
+var div = d3.select("body").append("div") 
+    .attr("class", "tooltip")
+    .style("opacity", 0);
+
+// d3.json("counties16.json", function(error, data) {
+//   d3.selectAll("svg").append("path")
+//     .data(topojson.mesh(data, data.objects.counties))
+//     .attr("d", path)
+//     .attr("class", "counties");
+//   });
+// TOOLTIP CREATION //
+
+// var tooltip = mapcontainer.append("div")
+//       .style("background-color", "White")
+//       .style("padding", "5px")
+//       .style("width", "100px")
+//       .style("position", "absolute")
+//       .style("border-radius", "8px")
+//       .style("font-family", "sans-serif")
+//       .style("font-size", "12px")
+//       .style("z-index", "10")
+//       .style("visibility", "hidden");  
+
+
+// var tooltipH = d3.select("body")
+//       .append("div")
+//       .style("background-color", "White")
+//       .style("padding", "5px")
+//       .style("width", "100px")
+//       .style("position", "absolute")
+//       .style("border-radius", "8px")
+//       .style("font-family", "sans-serif")
+//       .style("font-size", "12px")
+//       .style("z-index", "10")
+//       .style("visibility", "hidden");  
